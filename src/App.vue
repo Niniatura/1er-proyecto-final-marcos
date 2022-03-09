a676d42ce58345718fd99b10d1fe11a9b757a0ff
Niniatura <neli.marcos@gmail.com>
2022-03-09 09:42:26 -0300

    Products JSON

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,23 @@
     <v-main>
       <nav-header></nav-header>
       <form-registro></form-registro>
+      <product-list></product-list>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import FormRegistro from './components/FormRegistro.vue';
-import NavHeader from './components/NavHeader.vue'
+import NavHeader from './components/NavHeader.vue';
+import ProductList from './components/ProductList.vue'
 
 export default {
   name: 'App',
 
   components: {
     NavHeader,
-    FormRegistro
+    FormRegistro,
+    ProductList
   },
 
   data: () => ({
---
+++ src/components/productsJSON.js
@@ -0,0 +1,102 @@
+export default items = [
+    {
+        id:'1',
+        title: "Crunchy Bento box",
+        description: "Aliquam erat volutpat. Ut fermentum orci eget dolor accumsan, sit amet dapibus lacus lobortis.",
+        price: "750",
+        stock:34,
+        image:"../../public/images/sushi-1.jpeg"
+    },
+
+    {
+        id:'2',
+        title: "Nigiri Bento box",
+        description: "Ut fermentum orci eget dolor accumsan, sit amet dapibus lacus lobortis.Aliquam erat volutpat.",
+        price: "900",
+        stock:15,
+        image:"../../public/images/sushi-2.jpeg"
+    },
+
+    {
+        id:'3',
+        title: "Peruvian Bento box",
+        description: "Nam dignissim dolor ut arcu malesuada, eget elementum felis eleifend. Suspendisse sollicitudin ex odio.",
+        price: "840",
+        stock:12,
+        image:"../../public/images/sushi-3.jpeg"
+    },
+
+    {
+        id:'4',
+        title: "Tempura Bento box",
+        description: "Suspendisse sollicitudin ex odio. Nam dignissim dolor ut arcu malesuada, eget elementum felis eleifend.",
+        price: "670",
+        stock:28,
+        image:"../../public/images/sushi-4.jpeg"
+    },
+
+    {
+        id:'5',
+        title: "Salmon Bento box",
+        description: "Sed auctor diam tellus, at eleifend leo imperdiet nec. Etiam elementum libero mauris.",
+        price: "1100",
+        stock:28,
+        image:"../../public/images/sushi-5.jpeg"
+    },
+
+    {
+        id:'6',
+        title: "Meat Bento box",
+        description: "Etiam elementum libero mauris. Sed auctor diam tellus, at eleifend leo imperdiet nec.",
+        price: "1200",
+        stock:23,
+        image:"../../public/images/sushi-6.jpeg"
+    },
+
+    {
+        id:'7',
+        title: "Extras",
+        description: "Soya sauce.",
+        price: "350",
+        stock:35,
+        image:"../../public/images/salsa-soja.jpg"
+    },
+
+    {
+        id:'8',
+        title: "Extras",
+        description: "Teriyaki sauce",
+        price: "350",
+        stock:15,
+        image:"../../public/images/salsa-teriyaki.jpg"
+    },
+
+    {
+        id:'9',
+        title: "Extras",
+        description: "Biterseweet sauce.",
+        price: "350",
+        stock:25,
+        image:"../../public/images/salsa-agridulce.jpg"
+    },
+
+    {
+        id:'10',
+        title: "Extras",
+        description: "Ginger.",
+        price: "350",
+        stock:14,
+    },
+
+    {
+        id:'11',
+        title: "Extras",
+        description: "Wasabi.",
+        price: "350",
+        stock:46,
+        image:"../../public/images/wasabi.jpg"
+    }
+
+];
+
+export default data;
